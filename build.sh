5f2b04d654b6ecb2d772b98a5ffb15b334c44997
aryansaini-71 <aryan9817692114@gmail.com>
2026-03-26 15:22:15 -0600

    eighth commit

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@ set -e
 # 1. Clone Flutter (shallow)
 git clone --depth 1 https://github.com/flutter/flutter.git -b stable
 
-# 2. Add to PATH (using absolute path for safety)
+# 2. Add to PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 # 3. Suppress analytics
 export FLUTTER_NO_ANALYTICS=1
 
-# 4. Build
+# 4. Build (Simplified & Reordered)
 ./flutter/bin/flutter config --enable-web
 ./flutter/bin/flutter pub get
-./flutter/bin/flutter build web --release --web-renderer canvaskit --no-tree-shake-icons
+# We moved '--release' to the front and removed the problematic renderer flag for now
+./flutter/bin/flutter build web --release
 
 # 5. Move to public
 mkdir -p public
