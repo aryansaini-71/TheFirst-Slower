4bd29079a2e7b6efe80642009c5a640f078a9b04
aryansaini-71 <aryan9817692114@gmail.com>
2026-03-26 15:49:19 -0600

    FIxing Img errors

--- lib/main.dart
+++ lib/main.dart
@@ -83,8 +83,9 @@ class _SlowerHomeState extends State<SlowerHome> {
 
         setState(() {
           // FIXED: Using string interpolation ${} instead of +
+          // Use the 'cors-proxy.htmldriven.com' bridge - it is very friendly to Flutter
           memeUrl =
-              "https://api.allorigins.win/raw?url=${Uri.encodeComponent(selectedMeme['url'])}";
+              "https://cors-proxy.htmldriven.com/?url=${Uri.encodeComponent(selectedMeme['url'])}";
           memeTitle =
               "${selectedMeme['title'].toUpperCase()} (${selectedMeme['ups']} 👍)";
         });
