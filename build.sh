efa3d196c06337dd24dea85615def391f738267c
aryansaini-71 <aryan9817692114@gmail.com>
2026-03-26 15:39:53 -0600

    Adding Fixes

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -e
 
-# 1. Clone Flutter (shallow)
+# 1. Clean up any old flutter folder so the clone doesn't fail
+rm -rf flutter
+
+# 2. Clone Flutter (shallow)
 git clone --depth 1 https://github.com/flutter/flutter.git -b stable
 
-# 2. Add to PATH
+# 3. Add to PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
 
-# 3. Suppress analytics
+# 4. Suppress analytics
 export FLUTTER_NO_ANALYTICS=1
 
-# 4. Build (Simplified & Reordered)
+# 5. Build
 ./flutter/bin/flutter config --enable-web
 ./flutter/bin/flutter pub get
-# We moved '--release' to the front and removed the problematic renderer flag for now
 ./flutter/bin/flutter build web --release
 
-# 5. Move to public
+# 6. Move to public
 mkdir -p public
 cp -r build/web/* public/
